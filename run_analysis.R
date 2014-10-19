509153902bd7b8abddfead6c208e5e1e67337dc3
root <edward>
2014-10-18 21:52:39 -0700

    start of assignment 2

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+#
+
+fetch_data <- function() {
+   zipData <- './UCI%20HAR%20Dataset.zip'
+   if(!file.exists('UCI HAR Dataset') & !file.exists('UCI%20HAR%20Dataset')) {
+      print('Downloading activity.zip ....')   
+      download.file(url='https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', destfile=zipData, method='curl')
+   }
+   if(!file.exists('UCI HAR Dataset')) {
+      print('Unzip UCI HAR Dataset.zip ....')  
+      unzip(zipfile=zipData, overwrite=TRUE)
+   }
+}
+
