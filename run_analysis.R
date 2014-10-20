7f0ca5ba3cec35558a595f30c3c4d98f43e05cf1
root <edward>
2014-10-19 22:47:42 -0700

    minor improvement

--- run_analysis.R
+++ run_analysis.R
@@ -21,9 +21,19 @@ fetch_data <- function() {
    }
 }
 
-# 
+# The goal is to create a single table of all data on each subject. One observation is on a subject per timestamp.
 setwd("./UCI HAR Dataset")
+
 activityLabels <- read.table(file='activity_labels.txt', skipNul=TRUE, stringsAsFactors=FALSE)
+mergedata <- function(filename) {
+   requirethat(!is.na(filename), 'Filename is absent')
+   data_train <-read.table(file=paste('train//', filename, '_train.txt', sep=''), skipNul=T, stringsAsFactors=F)
+   data_test <-read.table(file=paste('test//', filename, '_test.txt', sep=''), skipNul=T, stringsAsFactors=F)
+   rbind2(x=data_train, y=data_test)
+}
+subject_set <- function() {
+
+}
 
 merge_testtrain <- function() {
    
@@ -36,6 +46,11 @@ merge_testtrain <- function() {
 savetidydata <- function() {
 
 
+}
+
+# Util function
+requirethat <- function(predicate, message) {
+        if(!(predicate)) stop(message) 
 }
 
 # Main
