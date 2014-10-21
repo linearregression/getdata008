ae1b167431362dc120054fc8da0d79c9122f371b
root <edward>
2014-10-20 18:48:23 -0700

    check in for now

--- run_analysis.R
+++ run_analysis.R
@@ -93,14 +93,12 @@ transform_activityId <- function(dataset) {
 # We only care about columns that have either mean or stardard deviations
 filter_data <- function(dataset) {
      requirethat(is.data.frame(dataset), 'Dataset cannot be absent or NULL')
-     print('ok filterdata check')
      colsextracted <- colnames(dataset)
      cols_mean <- grep(pattern="*-mean\\(\\)*", colsextracted, ignore.case=T)
      cols_std <- grep(pattern="*-std\\(\\)*", colsextracted, ignore.case=T)
      col_index <- union(c(1,2,cols_mean),cols_std)
      colsextracted <- dataset[col_index]
      col <- cleanse_colname(colsextracted)
-     print('ok filterdata cleanse colname')
      # assign new labelled columns
      colnames(colsextracted) <- col
      rm(list=c('col','cols_mean','cols_std', 'col_index'))
